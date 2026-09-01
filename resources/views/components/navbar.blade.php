b7d95e00e6e1a3956dc68b0815acdc10981cbfa6
allanbj <allanbjdaf@gmail.com>
2026-09-01 14:48:05 +0000

    Mise à jour des vues, des routes et ajout des images des partenaires

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -913,7 +913,7 @@
     {{-- Logo --}}
     <a href="{{ route('index') }}" class="nav-logo">
         <div class="nav-logo-icon" style="background: none; border: none; border-radius: 0; padding: 0; box-shadow: none;">
-            <img src="{{ asset('images/264.png') }}"
+            <img src="{{ asset('images/r2c.png') }}"
                 alt="Logo JEFIE Paris 2026"
                 style="height: 60px; width: 200px; display: block; border-radius: 0; border: none; background: transparent;">
         </div>
